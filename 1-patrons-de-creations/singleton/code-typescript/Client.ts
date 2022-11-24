796b65c6c4bcdc010ef561caf36bc10f1a90d7e7
ngimdock <ngimdockzemfack@gmail.com>
2022-11-24 07:17:28 +0100

    Vérification que la Bd singleton renvoie bien la même instance

--- 1-patrons-de-creations/singleton/code-typescript/Client.ts
+++ 1-patrons-de-creations/singleton/code-typescript/Client.ts
@@ -3,6 +3,12 @@ import { Database } from "./database/database.js";
 const client = async () => {
   const database: Database = Database.getInstance("localhost", 3000);
 
+  const database2: Database = Database.getInstance();
+
+  const database3: Database = Database.getInstance();
+
+  console.log(database === database2);
+
   await database.connection();
 
   const allUsers = await database.query("SELECT * FROM Users");
